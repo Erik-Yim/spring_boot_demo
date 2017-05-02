cfe650e696f1e3c7b9af1b6298c1754190f49ef1
Erik Yim <erik_yim@aliyun.com>
2017-05-02 11:26:54 +0800

    在配置文件中引用其他属性配置的用法

--- web-demo/src/main/java/org/erik/spring/boot/demo/controller/HelloController.java
+++ web-demo/src/main/java/org/erik/spring/boot/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package org.erik.spring.boot.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 
+
 @RestController
 public class HelloController {
 
+    @Value("${people.age}")
+    //当application.properties和当application.yml都存在是优先读取properties文件，
+    // 并且idea支持在编辑器中直接查看properties的文件属性，yml文件不支持
+    private int age;
+
+    @Value("${people.name}")
+    private String name;
+
+    @Value("${content}")
+    private String content;
+
+    @Value("${people.content}")
+    private String peopleContent;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "hello";
+        return "hello "  + name + " " + age + " content " + content +
+                " people content = " + peopleContent;
     }
 }
