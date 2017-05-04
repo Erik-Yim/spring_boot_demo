1a56337541cbf94c1f1752b1d1ca116aa6d0fe49
Erik Yim <erik_yim@aliyun.com>
2017-05-04 16:17:42 +0800

    lombok 支持

--- web-demo/src/main/java/org/erik/spring/boot/demo/vo/People.java
+++ web-demo/src/main/java/org/erik/spring/boot/demo/vo/People.java
@@ -1,7 +1,11 @@
 package org.erik.spring.boot.demo.vo;
 
+import lombok.Data;
+import lombok.NonNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * Created by Erik_Yim on 2017/5/2.
@@ -10,37 +14,39 @@ import org.springframework.stereotype.Component;
 //ConfigurationProperties这个加上去配置文件的前缀而已省略
 @Component
 @ConfigurationProperties(prefix = "people")
-public class People {
+//https://projectlombok.org lombok 简化java开发 http://jnb.ociweb.com/jnb/jnbJan2010.html
+public @Data class People {
 
     //age 可以是String或者数字都可以，属性文件不区分类型
     private String age; //和配置文件中属性名相同即可，就是prefix + fieldname
 
     private String name;
 
-    public String getAge() {
-        return age;
-    }
+//    public String getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(String age) {
+//        this.age = age;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
-    public void setAge(String age) {
-        this.age = age;
-    }
+//    @Override
+//    public String toString() {
+//        final StringBuilder sb = new StringBuilder("{");
+//        sb.append("\"age\":\"")
+//                .append(age).append('\"');
+//        sb.append(",\"name\":\"")
+//                .append(name).append('\"');
+//        sb.append('}');
+//        return sb.toString();
+//    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("{");
-        sb.append("\"age\":\"")
-                .append(age).append('\"');
-        sb.append(",\"name\":\"")
-                .append(name).append('\"');
-        sb.append('}');
-        return sb.toString();
-    }
 }
