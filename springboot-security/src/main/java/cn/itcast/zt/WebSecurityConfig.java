5c4abc2f7d085336afa9b264031ae390437bdd7a
Erik Yim <erik_yim@aliyun.com>
2017-05-10 15:32:13 +0800

    CORS跨域请求demo

--- springboot-security/src/main/java/cn/itcast/zt/WebSecurityConfig.java
+++ springboot-security/src/main/java/cn/itcast/zt/WebSecurityConfig.java
@@ -14,10 +14,28 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
+
+//                   ANT通配符有三种：
+//
+//                   通配符	说明
+//               ?	匹配任何单字符
+//               *	匹配0或者任意数量的字符
+//               **	匹配0或者更多的目录
+//                   例子：
+//
+//                   URL路径	说明
+//               /app/*.x	匹配(Matches)所有在app路径下的.x文件
+//               /app/p?ttern	匹配(Matches) /app/pattern 和 /app/pXttern,但是不包括/app/pttern
+//               /**/example	匹配(Matches) /app/example, /app/foo/example, 和 /example
+//               /app/**/dir/file.*	匹配(Matches) /app/dir/file.jsp, /app/foo/dir/file.html,/app/foo/bar/dir/file.pdf, 和 /app/dir/file.java
+//               /**/*.jsp	匹配(Matches)任何的.jsp 文件
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+//                .antMatchers("/", "/home","/js/**").permitAll()
+                .antMatchers("/", "/home","/**/*.js").permitAll() //允许所有人都可以访问的路径
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -25,7 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .permitAll()
                 .and()
                 .logout()
-                .permitAll() ;
+                .permitAll();
     }
 
     @Autowired
@@ -33,7 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         authenticationManagerBuilder
                 .inMemoryAuthentication()
                 .withUser("user")
-                .password("password")
+                .password("11111")
                 .roles("USER") ;
     }
 }
