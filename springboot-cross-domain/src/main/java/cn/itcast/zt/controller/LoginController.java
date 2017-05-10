e76a956716053c113d9a5ec21e15407296633a0d
Erik Yim <erik_yim@aliyun.com>
2017-05-10 10:22:30 +0800

    CORS跨域请求demo

---
+++ springboot-cross-domain/src/main/java/cn/itcast/zt/controller/LoginController.java
@@ -0,0 +1,10 @@
+package cn.itcast.zt.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Erik_Yim on 2017/5/9.
+ */
+@Controller
+public class LoginController {
+}
